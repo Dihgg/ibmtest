92d34be9be5d5c5727d7e80c3cb70eccdfbb960b
Diego Lopes <dihgg@dihgg.com>
2019-10-09 01:45:41 -0300

    tidy up

--- api/app.js
+++ api/app.js
@@ -2,22 +2,12 @@
 const express 		= require('express');
 const app 			= express();
 const bodyParser 	= require('body-parser');
-const mysql   		= require('mysql2');
-const env 			= process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json')[env];
 
 
 
 // APP - Definitions
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// DATABASE
-// var db = mysql.createConnection({
-// 	host     : config.database,
-// 	user     : config.username,
-// 	password : config.password
-// });
 
 
 // Database events
@@ -36,7 +26,7 @@ router.use(function(req, res, next) {
 router.use('/list', require('./routes/list').router);
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send('Funciona!');
 });
 
 // REGISTER ROUTES
