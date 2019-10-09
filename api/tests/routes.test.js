cfb047faec660d7b190cf27ed20c808381ccc5c9
Diego Lopes <diego@tinpix.com.br>
2019-10-09 09:35:59 -0300

    more api tests

--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -1,10 +1,23 @@
 const request   = require('supertest');
 const app   = require('../app');
 
-describe('get Endpoint', () => {
-    it('should get items', async () => {
-        const res = await request(app)
-        .get('/api/list/get')
-        expect(res.statusCode).toEqual(201);
-    });
+describe('Endpoints', () => {
+	it('should get items', async () => {
+		const res = await request(app)
+		.get('/api/list/get')
+		expect(res.statusCode).toEqual(201);
+	});
+	
+	it('should create a new item', async () => {
+		const res = await request(app)
+		.post('/api/list/add')
+		.send({
+			name		: "vinho",
+			qty		: 1,
+			checked	: false
+		})
+		expect(res.statusCode).toEqual(201)
+		expect(res.body).toHaveProperty('id');
+	});
+
 });
