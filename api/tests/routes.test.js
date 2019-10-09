6ecfa3f9e2e1c30f5a9d6d8e4d20a249c6582038
Diego Lopes <dihgg@dihgg.com>
2019-10-09 20:37:32 -0300

    testes na API

--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -20,4 +20,21 @@ describe('Endpoints', () => {
 		expect(res.body).toHaveProperty('id');
 	});
 
+	it('should update a item', async () => {
+		const res = await request(app)
+		.put('/api/list/edit/1')
+		.send({
+			name	: "abacaxi",
+			qty		: 2,
+			checked	: true
+		})
+		expect(res.statusCode).toEqual(201)
+		expect(Array.isArray(['value'])).toBe(true);
+	});
+
+	it('should delete a item', async () => {
+		const res = await request(app).delete('/api/list/delete/1');
+		expect({status: true}).toBeTruthy();
+	  });
+
 });
